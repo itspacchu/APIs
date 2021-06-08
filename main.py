5b51a7eb0b1097a2c56fd8ed57de1d129c4bb8bd
Pacchu <37984032+itspacchu@users.noreply.github.com>
2021-06-08 22:22:50 +0530

    forgot about procfile omg

--- main.py
+++ main.py
@@ -47,6 +47,6 @@ def upload_file():
                     })
         
 
-        
-if __name__ == '__main__':
-      app.run()
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
