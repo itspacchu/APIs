1978a6a07161e26d6574b563b52b0dc1532fc505
Pacchu <37984032+itspacchu@users.noreply.github.com>
2021-06-08 22:52:06 +0530

    port from env?

--- app.py
+++ app.py
@@ -47,5 +47,5 @@ def upload_file():
         
 
 if __name__ == "__main__":
-    #port = int(os.environ.get('PORT', 80))
-    app.run(host='0.0.0.0',port=80,debug=False)
+    port = int(os.environ.get('PORT', 80))
+    app.run(host='0.0.0.0',port=port,debug=False)
